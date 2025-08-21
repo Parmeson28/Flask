e87668dd4a71b8f9065c7a7b06358c8a1df1d2f8
Parmeson28 <tiagopassigern2802@gmail.com>
2025-08-21 08:06:43 -0300

    Atualização para o RENDER

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,28 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+
+load_dotenv('.env')
 
 app = Flask(__name__)
 
-from app.view import homepage
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['UPLOAD_FILES'] = r'statoc/data'
+
+db = SQLAlchemy(app)  
+
+migrate = Migrate(app, db)
+
+loginManager = LoginManager(app)
+loginManager.login_view = 'index'
+bcrypt = Bcrypt(app)
+
+from app.view import index
+from app.models import Contato
